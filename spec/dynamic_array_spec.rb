7e4064cc8809dab93353f6fe6accf15df79e566e
Petro Koriakin <petro@creditninja.com>
2021-03-09 11:03:22 +0200

    Add rubocop and more gems

---
+++ spec/dynamic_array_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative '../dynamic_array'
+
+RSpec.describe DynamicArray do
+  it 'does smth' do
+    expect(described_class.new).not_to be_nil
+  end
+end
